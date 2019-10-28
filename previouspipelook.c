846b4dbe8f1408a19b32e34da3948069d7ca1228
Arohi-Srivastav <44645140+Arohi-Srivastav@users.noreply.github.com>
2019-10-28 20:48:57 +0530

    Update previouspipelook.c
    Changed pipe structure

--- previouspipelook.c
+++ previouspipelook.c
@@ -1,18 +1,18 @@
 #include "./header.h"
 void lookup(char** strings)
 {
-	int in=dup(0);
-	int out=dup(1);                                            //storing initial stdout and stdin
 	int i=0;
 	int par[500];
-	for(int k=0;k<500;k++)
+	for(int k=0;k<500;k++)                                                          //helps in storing input properly
 		par[k]=500;
 	int s=0;
-	//int newin=dup(in);
 	par[0]=0;
 	s++;
+	int flag=0;
 	while(strings[i]!=NULL)
 	{
+		if(strings[i][0]=='&')
+			flag=1;
 		if(strings[i][0]=='|')
 		{
 			par[s]=i+1;
@@ -20,128 +20,299 @@ void lookup(char** strings)
 		}
 		i++;
 	}
-	int reading[s];
-	int writing[s];
-
-
-	for(int t=0; t < s; t++)
+	if (flag==1)
 	{
-		reading[t] = -1;
-		writing[t] = -1;
+		exec(strings);
 	}
-
-	int j;
-	int fileds[2];
-	for(j=0; j < s-1; j++)
+	else
 	{
-		int fileds[2];
-		pipe(fileds);
-		reading[j+1] = fileds[0];
-		writing[j] = fileds[1];
-	}
-	for(i=0;i<s;i++)
-	{
-		int piped[2];
-		pipe(piped);
-		char *instr[500];
-		int ink=0;
-		for(int j=par[i];j<par[i+1]-1;j++)
+		int in=dup(0);
+		int out=dup(1);                                            //storing initial stdout and stdin
+		int read_end[1000];
+		int write_end[1000];
+		for(int t=0; t < s; t++)
 		{
-			if(strings[j]!=NULL)
-			{
-				instr[ink]=strings[j];
-				ink++;
-			}
-			else
-			{
-				instr[ink]='\0';
-				ink++;
-				break;
-			}
-
-		}	
-		int p=fork();
-		if(p==0)
+			read_end[t] = -1;
+			write_end[t] = -1;
+		}
+		int j;
+		int pipes[1000];
+		for(j=0; j < s-1; j++)
 		{
-			//dup2(newin,0);
-			//close(newin);
-		/*	if(i!=s-1)
-			  {
-			  close(1);
-			  dup2(piped[1],1);
-			  if(i!=0)
-			  {
-			  close(0);
-			  dup2(piped[0],0);
+			pipe(pipes+2*j);
+			read_end[j+1] = 2*j;
+			write_end[j] = 2*j+1;
+		}
+		for(i=0;i<s;i++)
+		{
+			int in_r=0;
+			int out_r=0;
+			int app=0;
+			char *instr[500];
+			int ink=0;
+			int test =0;
+			char f[500],of[500];
+			int fd1=0;
+			int fd2=0;
+			int a=0,b=0,c=0;
+			for(int j=par[i];j<par[i+1]-1;j++)
+			{
+				if(strings[j]!=NULL)
+				{
+					if(strncmp(strings[j],"<",1)==0)
+						in_r=1;
+					else if(strncmp(strings[j],">>",2)==0)
+						app=1;
+					else if(strncmp(strings[j],">",1)==0 && app==0)
+						out_r=1;
+					else if(in_r==0&&out_r==0&&app==0)
+					{
+						instr[ink]=strings[j];
+						ink++;
+					}
+				}
+				if(in_r==1&&a==0)
+				{
+					strcpy(f,strings[j+1]);
+					a=1;
+				}
+				if(out_r==1&&b==0)
+				{
+					strcpy(of,strings[j+1]);
+					b=1;
+				}
+				if(app==1&&c==0)
+				{
+					strcpy(of,strings[j+1]);
+					c=1;
+				}
+				if(strings[j]==NULL)
+				{
+					instr[ink]='\0';
+					ink++;
+					break;
+				}
 
-			  }
-			  close(piped[0]);
-			close(piped[1]);
-			}
-			else
+			}	
+			if(strncmp(instr[0],"quit",4)==0)
 			{
-			if(i!=0)
-			{
-			close(0);
-			dup2(piped[0],0);
+				overkill();
+				exit(0);
 			}
-			close(1);
-			dup2(out,1);
-			close(piped[1]);
-			close(piped[0]);
-			close(out);
-			}*/
+			else if(strncmp(instr[0],"setenv",6)==0)
+			{
+				if(instr[1]==NULL || instr[3] != NULL)
+				{
+					printf("Invalid number of arguments to setenv\n");
+				}
+				else
+				{
+					if (instr[2]==NULL)
+					{
+						int s=setenv(instr[1],"",1);                  //one for setting overwrite flag
+						if(s==-1)
+							perror("setenv");
+					}
+					else
+					{
+						int s=setenv(instr[1],instr[2],1);
+						if(s==-1)
+							perror("setenv");
+					}
+				}
 
-			if(writing[i] != -1)
-			{
-				close(1);
-				dup2(writing[i],1);
 			}
+			else if(strncmp(instr[0],"unsetenv",8)==0)
+			{
+				if (instr[1]==NULL || instr[2]!=NULL)
+				{
+					printf("Invalid number of arguments to setenv\n");
+				}
+				else 
+				{
+					if (getenv(instr[1])==NULL)
+					{
+						printf("The environment variable does not exist\n");
+					}
+					else
+					{
+						unsetenv(instr[1]);
+					}
+				}
+			}
+			else if(strncmp(instr[0],"overkill",8)==0)
+			{
+				overkill();
+			}
+			else if(strncmp(instr[0],"jobs",4)==0)
+			{
+				listjobs();
+			}
+			else if(strncmp(instr[0],"kjob",4)==0)
+			{
+				kjob(instr);
+			}
+			else if(strncmp(instr[0],"fg",2)==0)
+				fg(instr);
+			else if(strncmp(instr[0],"bg",2)==0)
+				bg(instr);
 
-			if(reading[i] != -1)
-			{
-				close(0);
-				dup2(reading[i],0);
-			}
-			/*pr++;	
-			int count=0;
-			while(strings[count]!=NULL)
-				count++;
-			*//*if(strncmp(instr[0],"ls",2)==0)
-				ls(instr);
 			else if(strncmp(instr[0],"cd",2)==0)
-				cd(instr,ink);
-			else if(strncmp(instr[0],"pwd",3)==0)
-				pwd();
-			else if(strncmp(instr[0],"echo",4)==0)
-				echo(instr,ink);
-			else if(strncmp(instr[0],"pinfo",5)==0)
-				pinfo(instr);
-			else if(strncmp(instr[0],"ps",2)==0)
-				ps();
-			else if(strncmp(instr[0],"history",7)==0)
-				hist(strings);
-			else
-			{*/
-				execvp(instr[0],instr);
-			//}
-
-		}
-		else
-		{
-			wait(NULL);
-			//close(piped[1]);
-		//	close(piped[0]);
-			close(writing[i]);
-
-			if(i > 0)
 			{
-				close(reading[i]);
+				cd(instr,ink);
+			}
+			else if(strncmp(instr[0],"cronjob",7)==0)
+			{
+				cron(instr);
+				
+			}
+			else
+			{
+
+
+				int p=fork();
+				if(p==0)
+				{
+
+					if(in_r==1)
+					{
+						fd1=open(f, O_RDONLY);
+						if(fd1==-1)
+						{
+							printf("The input file was not found");
+							exit(0);
+						}
+						test = dup2(fd1,0);
+						if(test == -1)
+						{
+							perror("dup error");
+							return ;
+						}
+
+						close(fd1);
+					}
+					if(out_r==1)
+					{
+						fd2=open(of,O_CREAT | O_WRONLY | O_TRUNC, 0644);
+						test = dup2(fd2,1);
+						if(test == -1)
+						{
+							perror("dup error");
+							return ;
+						}
+
+						close(fd2);
+					}
+					if(app==1)
+					{
+						fd2=open(of,O_CREAT | O_WRONLY | O_APPEND, 0644);
+						test = dup2(fd2,1);
+						if(test == -1)
+						{
+							perror("dup error");
+							return ;
+						}
+
+						close(fd2);
+					}
+
+					if(app!=1 && out_r!=1 &&write_end[i] != -1)
+					{
+						dup2(pipes[write_end[i]],1);
+					}
+					if(in_r!=1 && read_end[i] != -1)
+					{
+						dup2(pipes[read_end[i]],0);
+					}
+
+					if(write_end[i] != -1 || read_end[i] != -1) 
+					{
+						for(int i_p = 0; i_p < 2*(s-1); i_p++) 
+						{
+							close(pipes[i_p]);
+						}
+					}
+					if(strncmp(instr[0],"ls",2)==0)
+					{
+						ls(instr);
+						exit(0);
+					}
+					else if(strncmp(instr[0],"pwd",3)==0)
+					{
+						pwd();
+						exit(0);
+					}
+					else if(strncmp(instr[0],"ps",2)==0)
+					{
+						ps();
+						exit(0);
+					}
+					else
+					{
+						int tt=0;
+						signal(SIGTSTP, SIG_DFL);
+						tt=execvp(instr[0],instr);
+						if (tt==-1)
+							perror("Execution of commmand failed");
+					}
+
+
+
+				}
+
+				else
+				{
+					close(fd1);
+					close(fd2);
+					if(write_end[i] != -1)
+						close(pipes[write_end[i]]);
+					waitpid(p, NULL, WUNTRACED);
+					if(getpgid(p) >= 0)
+					{
+						st new;
+						new.pid=p;
+						strcpy(new.name,strings[0]);
+						char *PID[] = {NULL, NULL};
+						char Pid[100];
+						sprintf(Pid, "%d", p);
+						PID[0] = Pid;
+						char d=pinfo(PID);
+						if(d=='R')
+						{
+							strcpy(new.state,"Running");
+						}
+						else if(d=='S')
+						{
+							strcpy(new.state,"Sleeping");
+						}
+						else if(d=='T')
+						{
+							strcpy(new.state,"Stopped");
+						}
+						int t=0;
+						while(list[t].pid!=-1)
+							t++;
+						list[t]=new;
+
+
+					}
+
+				}
 			}
 		}
+		for (i=0;i<2*(s-1);i++)
+		{
+			close(pipes[i]);
+		}
+
+		dup2(in,0);
+		dup2(out,1);
+		close(in);
+		close(out);
+
 	}
-	dup2(in,0);
-	//dup2(out,1);
-	close(in);
+
+
 }
+
 
